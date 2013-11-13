6a267ad8109dad20ac9ff1e8e2734e83b70561d5
Juan Manuel Serruya <shuank@gmail.com>
2013-11-13 17:45:01 +0100

    added activityBar and android support library

--- src/com/jmserruya/dummyapp/DisplayMessageActivity.java
+++ src/com/jmserruya/dummyapp/DisplayMessageActivity.java
@@ -1,15 +1,15 @@
 package com.jmserruya.dummyapp;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends Activity {
+public class DisplayMessageActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
